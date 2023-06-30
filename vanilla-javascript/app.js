998430b9b65a83bd00d574d64763dc07f5a45e2e
xudegloss <ajouunihospitalrn@naver.com>
2023-06-30 17:18:28 +0900

    js로crud 구현하기 시도

---
+++ vanilla-javascript/app.js
@@ -0,0 +1,6 @@
+const todoContainer = document.getElementById("todo__container");
+const submitBtn = document.querySelector(".submit_btn");
+
+const todoFunc = () => {
+  const todo = document.getElementById("todo__list").value;
+};
